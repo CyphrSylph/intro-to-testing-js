47a1b40744266969853f08c2dc1614cd9579b808
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-06-13 15:05:18 -0500

    Exercise 1 Completed

--- code.js
+++ code.js
@@ -1,4 +1,19 @@
 // helloWorld function 
-function helloWorld() {
-    return "Hello, World!";
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+
+// helloWorld expression
+const helloWorld = function() {
+    return 'Hello, World!';
+}
+helloWorld();
+
+// sayHello expression
+const sayHello = function () {
+}
+
+// foo function
+function foo() {
+	return 2;
 }
